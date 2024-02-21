d366ae9cda69112f3d603afc19f9ad14097c2be4
sam <samshariat7@gmail.com>
2024-02-21 22:44:53 +0330

    fixing type error

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -2,13 +2,13 @@ import { Box, Container, Link, Text, useColorModeValue } from '@chakra-ui/react'
 export default function Footer() {
   return (
     <>
-    <Box w="full" py={5} backgroundColor={'blackAlpha.200'}>
+    <Container as="footer" w="full" py={5} backgroundColor={'blackAlpha.200'}>
       <Container flexDirection={'column'} maxW="container.lg" alignItems={'center'} display="flex" justifyContent="space-between" gap={4}>
        
         <Text>2023 © Venom ID</Text>
 
       </Container>
-    </Box>
+    </Container>
     </>
   );
 }
